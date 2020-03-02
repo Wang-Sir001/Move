162ca1fc7c96f04ef8d1f8d50c743dfe6036bc0b
Wang-Sir001 <wangjiangweis@qq.com>
2020-03-02 08:46:33 +0800

    Initial commit

---
+++ app/src/main/java/com/bawei/move/MainActivity.java
@@ -0,0 +1,19 @@
+package com.bawei.move;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * data:${DATE}
+ * author:王江伟(${USER})
+ * function:
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
